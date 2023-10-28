62deb1e382a9df98c684f775cb311423714d0eb9
Phillip Bronzel <bronzel.phillip@gmail.com>
2023-10-28 15:17:11 +0200

    change font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,20 +1,11 @@
-import {
-  JetBrains_Mono as FontMono,
-  Inter as FontSans,
-  Syne,
-} from 'next/font/google';
+import { Syne } from 'next/font/google';
+import { GeistMono as FontMono, GeistSans as FontSans } from 'geist/font';
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
+export const fontSans = FontSans;
 
 export const fontSerif = Syne({
   subsets: ['latin'],
   variable: '--font-serif',
 });
 
-export const fontMono = FontMono({
-  subsets: ['latin'],
-  variable: '--font-mono',
-});
+export const fontMono = FontMono;
